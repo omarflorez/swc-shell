6d55129afdeb44c1e86d4eb0389d229821eb1287
omarflorez <omar.florez@usu.edu>
2013-07-17 13:34:18 -0400

    Adding comment to clarify when the main function is called.

--- avg_discharge_rate.py
+++ avg_discharge_rate.py
@@ -35,6 +35,8 @@ def main():
     avg_discharge_rates = get_avg_discharge_rates(data)
     for station in avg_discharge_rates:
         print(station + ' ' +  str(avg_discharge_rates[station]))
-
+# if this script is executed from the command line
+# run the main() function;
+# if its imported into another file, dont
 if __name__ == '__main__':
     main()
